76134e4d6c248b734b7c76b4a9841a1aee765ca6
Albert Cui <aqc2109@columbia.edu>
2015-05-30 20:44:18 -0400

    make efficient

--- cracking/ch9_9-1-steps.py
+++ cracking/ch9_9-1-steps.py
@@ -1,4 +1,4 @@
-def findSteps(n, choices):
+def findStepsNaive(n, choices):
     """
     Finds the number of permuations to make n steps, given
     a set of possible choices for each move
@@ -11,9 +11,40 @@ def findSteps(n, choices):
         if remain == 0:
             count += 1
         elif remain > 0:
-            count += findSteps(remain, choices)
+            count += findStepsNaive(remain, choices)
     return count
 
 choices = {1, 2, 3}
+target_steps = 5
+print findStepsNaive(target_steps, choices)
 
-print findSteps(3, choices)
+def findStepsEfficient(n):
+    if n < 0:
+        return "error"
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+
+    # Can use variables
+    # prev1 = 4
+    # prev2 = 2
+    # prev3 = 1
+    # Or a list/array
+    pi = [1, 2, 4]
+    for i in range (4, n + 1):
+        # steps = prev1 + prev2 + prev3
+        # prev3 = prev2
+        # prev2 = prev1
+        # prev1 = steps
+        steps = sum(pi)
+        pi.pop(0)
+        pi.append(steps)
+
+    return pi[2]
+
+print findStepsEfficient(target_steps)
